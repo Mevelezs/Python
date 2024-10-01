84130e1d1b0eb361a3c120caf173ae71f9c6f783
mevelezs <serketh7@gmail.com>
2024-10-01 00:45:18 -0500

    Lists

--- python/listas.py
+++ python/listas.py
@@ -1,20 +1,35 @@
 monts = ["January", "February", "March", "April"];
 
 print(monts[0], monts[2])
-
+print(monts[:])
 print(monts)
+print(monts[2:])
+print(monts[1:3])
+print(monts[:2])
+print(monts[-3])
+
+# Agregar
+monts.append("May") # Agrega al filnal
+monts.insert(2, "June") # Agrega en un posicion dad
+monts.extend(["July", "August", "September"]) # Concatena las listas
+print(monts)
+
+#tamaño
+print(len(monts))
+
+#Indice por nombre
+print(monts.index("August"))
+
+# Verifica la existencia del elemnto in()
+print("August" in monts)
 
 # sort
 monts.sort()
 print(monts)
 
-# Agregar
-monts.append("May")
-print(monts)
-
 # Eliminar
-del monts[3]
-print(monts)
+del monts[2]
+print(monts, '----->')
 
 # Eliminar último
 monts.pop()
@@ -25,9 +40,11 @@ monts.pop(0);
 print(monts)
 
 # Eliminar por nombre
-
 monts.remove("January")
 print(monts)
+
+# Repetidor
+print(monts * 3)
 
 
 
