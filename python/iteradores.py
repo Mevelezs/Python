5c60e2aeb439a322f6f16c76f17d1d9b4ca93c08
mevelezs <serketh7@gmail.com>
2024-11-05 06:59:38 -0500

    for

--- python/iteradores.py
+++ python/iteradores.py
@@ -9,3 +9,8 @@ print(len(fruits))
 # range(init, length, increment)
 for x in range(0, len(fruits), 1):
   print(f"0.{x + 1} fruit")
+
+
+# recorriendo string
+for i in "caracter":
+  print(i, end="")
--- python/py.py
+++ python/py.py
@@ -1,1 +1,13 @@
-print("haello")
+faren = 10
+
+print(f"{faren} grados fahrenheit son {(float (faren) - 32)*( 5/9):.1f} grados centigrados")
+numero = 123.456
+
+# Imprimir con dos cifras significativas
+print(f"{numero:.2f}")
+
+R = float (input())
+W = (R * 0.19) + R
+T = R * 0.1
+U = R + W + T
+print(f"Total consumido: COP$ {R}\nValor IVA: COP$ {W}\nValor propina: COP$ {T}\nA pagar: COP$ {U}")
